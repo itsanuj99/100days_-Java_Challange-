d046d6a14eca8049954d1244f94fc0ff8b9cf43e
anujverma8395 <itsanuj20@gmail.com>
2026-07-08 22:13:23 +0530

    Day 7: For loop practice

---
+++ day_07/Num.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Num
+{
+    public static void main(String args[])
+    {
+        Scanner sc= new Scanner(System.in);
+        System.out.println(" Enter a number");
+        int num=sc.nextInt();
+
+        for(int i=1;i<=num;i++)
+        {
+            System.out.println(i);
+        }
+    }
+}
